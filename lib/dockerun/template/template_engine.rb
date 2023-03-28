7f3f9c1015b9e7b36a5d7beb04681a41c8ebb363
Chris <chris@antrapol.com>
2023-03-28 13:14:26 +0800

    Support generic template instead

--- lib/dockerun/template/template_engine.rb
+++ lib/dockerun/template/template_engine.rb
@@ -10,8 +10,14 @@ module Dockerun
 
       def self.available_templates
         avail = Dir.glob(File.join(template_root,"Dockerfile_*.erb"))
+
+        #userDir = File.join(Dir.home,".dockerun","template")
+        #if File.exist?(userDir)
+        #  avail += Dir.glob(userDir,"*.erb")
+        #end
+        
         avail.map! { |f| 
-          name = File.basename(f,".erb")
+          name = File.basename(f,File.extname(f))
           name.gsub("Dockerfile_","")
         }
       end
--- lib/dockerun/template/template_writer.rb
+++ lib/dockerun/template/template_writer.rb
@@ -11,7 +11,8 @@ module Dockerun
 
       class TemplateNotFound < StandardError; end
 
-      attr_accessor :image, :user_group_id, :user_group_name, :user_id, :user_login
+      attr_accessor :image, :user_group_id, :user_group_name, :user_id, :user_login, :maintainer, :image_base
+      attr_writer :user_configurables
 
       def self.instance(template)
         tmp = template.to_s.downcase
@@ -32,9 +33,40 @@ module Dockerun
         @user_group_name = group[:group_name]
         @user_id = user[:user_id]
         @user_login = user[:login]
+        @image_base = :ubuntu
+      end
+
+      def user_configurables
+        fields = {
+          image: { desc: "Image name of the docker based on", required: true },
+          image_base: { desc: "Image is based on which OS? Assumption with be on Debian based. ", default: :ubuntu },
+          maintainer: { desc: "Maintainer of the Dockerfile", default: @maintainer }
+        }
+
+        if TR::RTUtils.on_linux?
+          f2 = {
+            user_group_id: { desc: "User group ID that shall be created in docker. Default to current running user's ID", default: @user_group_id.to_s },
+            user_group_name: { desc: "User group name that shall be created in docker. Default to current running user's group", default: @user_group_name },
+            user_id: { desc: "User ID that shall be created in docker. Default to current running user ID", default: @user_id.to_s },
+            user_login: { desc: "User login name that shall be created in docker. Default to current running user's login", default: @user_login }
+          }
+          fields.merge!(f2)
+        end
+        fields
       end
 
       def compile(&block)
+
+        if not_empty?(@user_configurables)
+          @user_configurables.each do |k,v|
+            begin
+              self.send("#{k}=", v)
+            rescue Exception => ex
+              STDERR.puts "Setting value exception : #{ex}"
+            end
+          end
+        end
+
         tmp = find_template
         cont = nil
         File.open(tmp,"r") do |f|
